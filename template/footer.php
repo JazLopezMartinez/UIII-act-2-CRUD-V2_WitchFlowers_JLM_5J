d31beeed5aef753dcaaef7819f978568e9fe13bc
JazminLopezMartinez <143547919+JazLopezMartinez@users.noreply.github.com>
2023-11-15 12:37:22 -0700

    Update footer.php

--- template/footer.php
+++ template/footer.php
@@ -1,7 +1,7 @@
 <footer class="container-fluid bg-dark fixed-bottom">
         <div class="row">
             <div class="col-md text-light text-center py-3">
-                Desarrollado por Jazmin Lopez
+                Desarrollado por Jazmin Lopez Martinez 5J
             </div>
         </div>
     </footer>
